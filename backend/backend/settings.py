e75e5c7981d82f8271d3bccaf238036b4dc3c621
sky-lester <lesterreandino@skydev.solutions>
2024-08-07 16:36:31 +0800

    connect frontend to backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'hello',
     'post'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
